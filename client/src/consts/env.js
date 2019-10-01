f66fe39c85b8505f4c2a901ca226a77c9346a0e0
rozalex <rozalex@gmail.com>
2019-10-01 09:18:50 +0300

    update env

--- client/src/consts/env.js
+++ client/src/consts/env.js
@@ -1,5 +1,5 @@
-// const DB_URL = "https://app-point.herokuapp.com";
-const DB_URL = "http://localhost:5000";
+const DB_URL = location.hostname === "localhost" || location.hostname === "127.0.0.1" ?
+    "http://localhost:5000" : "https://app-point.herokuapp.com";
 export {
     DB_URL
 };
