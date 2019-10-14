e42357578e4f26513c7ddd64a95d7797bbd5479f
rozalex <rozalex@gmail.com>
2019-10-14 11:16:14 +0300

    update appointment structure

--- server/models/appointment.model.js
+++ server/models/appointment.model.js
@@ -3,11 +3,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const appointmentsSchema = new Schema({
-  provider: { type: String, required: true },
-  username: { type: String, required: true },
-  description: { type: String, required: true },
-  duration: { type: Number, required: true },
   date: { type: Date, required: true },
+  time: { type: String, required: true },
+  duration: { type: Number, required: true },
+  description: { type: String, required: true },
+  status: { type: String, required: true },
+  buinsessid: { type: String, required: true },
+  customerid: { type: String, required: true },
 }, {
   timestamps: true,
 });
